878f8b84c229a863d5e61c8924eb7475030419a3
Aditya jain <jain13102000@gmail.com>
2019-04-09 00:42:27 +0530

    Add files via upload

---
+++ data_preprocessing_template.py
@@ -0,0 +1,42 @@
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the Dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3].values
+
+# Taking care of Missing data
+from sklearn.preprocessing import Imputer
+imputer=Imputer(missing_values = 'NaN',strategy = 'mean', axis=0)
+imputer=imputer.fit(x[:, 1:3])
+x[:, 1:3]=imputer.transform(x[:, 1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x=LabelEncoder()
+x[:,0]=labelencoder_x.fit_transform(x[:,0])
+onehotencoder=OneHotEncoder(categorical_features = [0])
+x=onehotencoder.fit_transform(x).toarray()
+labelencoder_y=LabelEncoder()
+y=labelencoder_y.fit_transform(y)
+
+# splitting the dataset into Training Set and Test Set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_x=StandardScaler()
+x_train=sc_x.fit_transform(x_train)
+x_test=sc_x.transform(x_test)
+y = y.reshape(-1 ,1)
+y_train = y_train.reshape(-1 ,1)
+y_test = y_test.reshape(-1 ,1)
+sc_y=StandardScaler()
+y_train=sc_y.fit_transform(y_train)
+y_test=sc_y.transform(y_test)
